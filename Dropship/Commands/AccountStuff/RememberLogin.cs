fa418cb00ace179bf4df0a858f924aab6e62378c
Chipseq <sztabekminecraft@gmail.com>
2025-01-31 16:54:11 +0100

    fix for password saving

--- Dropship/Commands/AccountStuff/RememberLogin.cs
+++ Dropship/Commands/AccountStuff/RememberLogin.cs
@@ -5,14 +5,13 @@ public class RememberLoginCommand : Command
 {
     public override string Name => "remember_login";
     public override CommandCategory Category => CommandCategory.AccountStuff;
-    public override string Description => "Saves your login credentials for later";
+    public override string Description => "Remembers your password";
     public override string Arguments => null;
 
     public override bool Execute(string[] args)
     {
         DepotDownloader.RememberLoginData = true;
-        DepotDownloader.EncryptLogin();
-        Console.WriteLine("Login credentials saved");
+        Console.WriteLine("Your password will be saved for later");
         return true;
     }
 }
